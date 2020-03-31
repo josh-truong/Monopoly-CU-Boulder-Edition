d85a607da9b5e8a5315d8dc2c1193f83af577d88
srirachaBoi <62912895+srirachaBoi@users.noreply.github.com>
2020-03-31 12:36:24 -0600

    Update Game.h

--- Game.h
+++ Game.h
@@ -2,13 +2,14 @@
 #define GAME_H
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Game
 {
     private:
-        int FirstDiceRoll;
-        int SecondDiceRoll;
+        int firstDiceRoll;
+        int secondDiceRoll;
         bool InJail;
         char Map[44][44];
         string player[4]; //<-- Max of 4 players
@@ -17,7 +18,15 @@ class Game
     public:
         void display_MapAndPlayer() const;
         int roll() const;
-        void move(int);
+        void move() const; //<-- call the roll function
+        void endGame(string quit_);
+        void endTurn(string end_);
+        void buy(char y_n);
+        // We must figure out how to bring in the other user... We also will have to create an option for a different offer
+        //Offer should include money/property
+        void trade(string playerName, int property, int offer_); 
+        
+        
     
 };
 
