cd131c7eea7193e3a6b1d55c1301cd04d3f42ed1
srirachaBoi <62912895+srirachaBoi@users.noreply.github.com>
2020-04-03 23:27:13 -0600

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,18 +1,23 @@
 #include "Game.h"
-// #include "Property.h"
-// #include "Player.h"
+#include "Property.h"
+#include "Player.h"
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
 #include <iomanip>
+#include <fstream>
+#include <sstream>
 //01001110 01101001 01100011 01100101 00100001 00100001 00100000 01000101 01100001 01110011 01110100 01100101 01110010 00100000 01000101 01100111 01100111 00101100 00100000 01101000 01110101 01101000 00111111 00001101 00001010 00100000 00100000 00100000 01011111 00001101 00001010 00100000 00100111 00100000 00100000 00100000 00100111 00001101 00001010 00100111 00100000 00100000 00100000 00100000 00100000 00100111 00001101 00001010 00100000 00100111 00100000 01011111 00100000 00100111 00001101 00001010 00100000
 using namespace std;
 
 Game::Game()
 {
-    int dice_1 = 0;
-    int dice_2 = 0;
+    dice_1 = 0;
+    dice_2 = 0;
     inJail = false;
+    
+    cout << "How many players? (2-4): " << endl;
+    cin >> numPlayers;
     
     //Initialized Tabula Rasa Board
     for(int i = 0; i < 11; i++)
@@ -49,7 +54,7 @@ Game::Game()
 void Game::move()
 {
     //This is where we will update players position
-    int i = 0;
+    int i = 10;
     int j = 10;
     int k = 0;
     int l = 0;
@@ -71,7 +76,11 @@ void Game::move()
         map[i][j][k][l] += " ";
     }
     map[i][j][k][l] += " ";
+    display_MapAndPlayer();
+}
 
+void Game::display_MapAndPlayer() const
+{
     //4d array - This allows for fixed position of character while knowing which player is on which square
         /*arr[i][j][k][l] <-- I and J represent the number of squares in a monopoly board (11 by 11)
             //k and l are the position of each player. This will not be change under any circumstance to change the player position use i and j
@@ -132,6 +141,44 @@ void Game::move()
     }
     cout << endl;
 }
+
+void Game::readPlayers()
+{
+    ifstream readPlayerFile;
+    string x_pos, y_pos, playerChar_, playerName;
+    readPlayerFile.open("player.csv");
+    for(int i = 0; i <= numPlayers; i++)
+    {
+        getline(readPlayerFile, x_pos, ',');
+        getline(readPlayerFile, y_pos, ',');
+        getline(readPlayerFile, playerChar_, ',');
+        if(i != 0)
+        {
+            cout << "(" << (i - 1) << ") Enter a player name: " << endl;
+            cin >> playerName;
+            
+            player[i - 1].setName(playerName);
+            player[i - 1].setPlayerChar(playerChar_);
+            player[i - 1].setPlayerPos(stoi(x_pos), stoi(y_pos));
+        }
+    }
+    // for(int i = 0; i < numPlayers; i++)
+    // {
+    //     cout << player[i].getName() << " (" << player[i].getPlayerChar() << ")" << endl;
+    //     cout << "Balance: " << player[i].getBalance() << endl;
+    //     cout << "(" << player[i].getPlayerPos_x() << ", " << player[i].getPlayerPos_y() << ")" << endl;
+    // }
+    readPlayerFile.close();
+}
+
+void Game::readProperty()
+{
+    
+}
+
+
+
+
 
 void Game::roll()
 {
