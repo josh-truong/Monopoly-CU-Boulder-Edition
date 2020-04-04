0eb6a8d90c8063c196cd7093fde4a8066757c3c5
srirachaBoi <62912895+srirachaBoi@users.noreply.github.com>
2020-04-03 23:26:01 -0600

    Update Player.h

--- Player.h
+++ Player.h
@@ -9,24 +9,31 @@ using namespace std;
 class Player
 {
     private:
-        int balance;
         string playerName;
+        string playerChar;
+        int balance;
         int playerPos_x;
         int playerPos_y;
         bool bankrupt;
+        
     public:
         Player();
         
         void setName(string playerName_); // Set player name
+        void setPlayerChar(string playerChar_);
         // NOTE: We can only set the balance and not update it(ADD/SUBTRACT)
         void setBalance(int balance_); 
         void setPlayerPos(int x, int y);
+        
         
         //We must only use this function when we have determined that the player is bankrupted or has decided to quit
         void setBankruptStatusTrue();
         
         string getName() const; //Get player name
+        string getPlayerChar() const;
         int getBalance() const; //Get balance
+        int getPlayerPos_x() const;
+        int getPlayerPos_y() const;
         bool getStatus() const; // We must use this function to check if the player can play
 };
 #endif
