184d87a8b309096adea9713880c937a5796fe1f6
srirachaBoi <62912895+srirachaBoi@users.noreply.github.com>
2020-04-03 23:26:47 -0600

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,8 +1,8 @@
 #ifndef GAME_H
 #define GAME_H
 
-// #include "Property.h"
-// #include "Player.h"
+/#include "Property.h"
+#include "Player.h"
 #include <iostream>
 #include <vector>
 #include <string>
@@ -15,15 +15,23 @@ class Game
         int dice_2;
         bool inJail;
         string map[11][11][2][2];
-        // Player player[4]; //<-- Max of 4 players
+        int numPlayers;
+        Player player[4]; //<-- Max of 4 players
         // vector<Property> property;
 
     public:
         Game();
         void display_MapAndPlayer() const;
+        void move();
+        
+        //File readers <-- There is no need to have user input filenames, but would be a 
+        //great idea if they want to customize their own board
+        void readPlayers();
+        // void readProperty();
+        
         void roll();
         // void doubleTurn(int dice_1, int dice_2);
-        void move();
+        
         // void endGame(string quit_);
         // void endTurn(string end_);
         // void buy(char y_n);
