f5e496a882ed05ae46fa1bd875df01adc6decdd4
srirachaBoi <62912895+srirachaBoi@users.noreply.github.com>
2020-04-03 22:16:08 -0600

    COMPLETED

--- Player.h
+++ Player.h
@@ -4,19 +4,29 @@
 #include <iostream>
 using namespace std;
 
+
+//PLAYER CLASS IS FINISHED
 class Player
 {
     private:
         int balance;
         string playerName;
-        
+        int playerPos_x;
+        int playerPos_y;
+        bool bankrupt;
     public:
         Player();
         
-        string setName(string playerName_); // Set player name
-        int setBalance(int balance_); //Set balance
+        void setName(string playerName_); // Set player name
+        // NOTE: We can only set the balance and not update it(ADD/SUBTRACT)
+        void setBalance(int balance_); 
+        void setPlayerPos(int x, int y);
         
-        void getName() const; //Get player name
-        void getBalance() const; //Get balance
+        //We must only use this function when we have determined that the player is bankrupted or has decided to quit
+        void setBankruptStatusTrue();
+        
+        string getName() const; //Get player name
+        int getBalance() const; //Get balance
+        bool getStatus() const; // We must use this function to check if the player can play
 };
 #endif
