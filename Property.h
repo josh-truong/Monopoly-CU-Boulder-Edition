ed65964d93e7499bbaec0660512e7097189018cc
srirachaBoi <62912895+srirachaBoi@users.noreply.github.com>
2020-04-04 18:20:10 -0600

    Update Property.h

--- Property.h
+++ Property.h
@@ -7,33 +7,47 @@ using namespace std;
 class Property
 {
     private:
+        //Variable for all properties
+        int propertyLocation;
         string propertyName;
+        
+        //Variable for only colored properties
         string owner;
-        int cost;
-        int houseCost;
+        int propertyCost;
+        int buildingCost;
         //{rent,house1,house2,house3,house4,hotel} <-- rent in the array is for rent when no house/hotel is available
         int rentArr[6];
         //Suggestion: we add how many buildings are built
+        string color;
         int numBuildings;
+        
+        //Variables for railroads
+        
         
         
     public:
         //Constructor
         Property();
         
-        //Modifiers
-        void setCost(int cost_);
-        void setRentAt(int i, int rent_);
-        void setOwner(string username_, string propertyName_);
+        // //Modifiers
+        void setPropertyLocation(int propertyLocation_);
         void setPropertyName(string propertyName_);
+        void setOwner(string username_);
+        void setPropertyCost(int propertyCost_);
+        void setBuildingCost(int buildingCost_);
+        void setRentAt(int i, int rent_);
+        void setColor(string color);
+        void setNumBuildings(int numBuildings_);
         
-        
-        //Accessors
-        // void getPropertyName(); <--Need more details on how to obtain property name
-        int getCost(string propertyName_);
-        int getRent(string propertyName_); //<--We need to determine how many houses * rent
-        int getHouseCost(string propertyName_);
-        string getOwner(string propertyName_);
-
+        // //Accessors
+        int getPropertyLocation();
+        string getPropertyName(); //<--Need more details on how to obtain property name
+        string getOwner();
+        int getPropertyCost();
+        int getBuildingCost();
+        int getListOfRent();
+        int getRent(); //<--We need to determine how many houses * rent
+        string getColor();
+        int getNumBuildings();
 };
 #endif
